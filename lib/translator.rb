1111509bcc4ce4ce882badc137d03c15c805c227
breezy-karma-0220 <lh62594@yahoo.com>
2018-12-17 19:13:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ def get_japanese_emoticon(file_path, emoticon)
         jap_emoticon = Japanese
       end
     end
-  # end
+  end
 
   if jap_emoticon == ""
     return "Sorry, that emoticon was not found"
